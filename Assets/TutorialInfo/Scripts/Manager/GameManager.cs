ae7f7ce1661be15c155ebf63d53c221d6a79f0ce
Vminhkiet <minhkietvo6@gmail.com>
2025-04-16 22:47:17 +0700

    add photon

---
+++ Assets/TutorialInfo/Scripts/Manager/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public GameObject player;
+    [Space]
+    public Transform spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // connect to photon server default
+        Debug.Log("Connecting");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected to Photon Master Server.");
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+        Debug.Log("We in a room");
+    }
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("We 're connected and in a room");
+
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+    }
+}
