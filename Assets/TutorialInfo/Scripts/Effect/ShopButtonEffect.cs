1b56dc1e998e291c1781632bfca6274df4cca5dc
Vminhkiet <minhkietvo6@gmail.com>
2025-04-24 00:59:25 +0700

    view main

---
+++ Assets/TutorialInfo/Scripts/Effect/ShopButtonEffect.cs
@@ -0,0 +1,32 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public RectTransform shopImage;
+    private float boundStrength = 10f;
+    private float boundDuration = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartBounceEffect();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartBounceEffect()
+    {
+        shopImage.DOLocalRotate(new Vector3(0, 0, 10), 0.1f)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutSine);
+    }
+}
