4e87f96b180f397b387812db1cc8e0103185f578
lamden000 <lamden150@gmail.com>
2025-05-09 14:43:49 +0700

    refine hex drawer

--- Assets/TutorialInfo/Scripts/MapDesign/HexCell.cs
+++ Assets/TutorialInfo/Scripts/MapDesign/HexCell.cs
@@ -6,24 +6,20 @@ public class HexCell : MonoBehaviour {
 
 	public Color color;
 
-    [SerializeField]
-    HexCell[] neighbors;
-
+	[SerializeField]
+	HexCell[] neighbors;
     void Awake()
     {
         if (neighbors == null || neighbors.Length != 6)
             neighbors = new HexCell[6];
     }
 
-    public void SetNeighbor(HexDirection direction, HexCell cell)
-    {
-        neighbors[(int)direction] = cell;
-        cell.neighbors[(int)direction.Opposite()] = this;
-    }
+    public HexCell GetNeighbor (HexDirection direction) {
+		return neighbors[(int)direction];
+	}
 
-    public HexCell GetNeighbor(HexDirection direction)
-    {
-        return neighbors[(int)direction];
-    }
-
+	public void SetNeighbor (HexDirection direction, HexCell cell) {
+		neighbors[(int)direction] = cell;
+		cell.neighbors[(int)direction.Opposite()] = this;
+	}
 }
